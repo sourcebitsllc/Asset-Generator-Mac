7c90d914c506051bf5742d1cb38e33c7b70512f7
Bader <b.h.alrazzaqa@hotmail.com>
2014-11-08 04:37:14 +0300

    FileSystemObserver slight rewrite and new API

--- XCAssetGenerator/FileSystemObserver.h
+++ XCAssetGenerator/FileSystemObserver.h
@@ -1,27 +1,37 @@
 //
 //  FileSystemObserver.h
-//  XCAssetGenerator
+//  FSEventSwiftTest
 //
 //  Created by Bader on 10/10/14.
-//  Copyright (c) 2014 Pranav Shah. All rights reserved.
+//  Copyright (c) 2014 Bader. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 typedef NS_ENUM(NSUInteger, FileSystemOperation) {
-    FileSystemCreation,
-    FileSystemDeletion,
-    FileSystemRename,
+    FileSystemDirectoryCreated,
+    FileSystemDirectoryDeleted,
+    FileSystemDirectoryRenamed,
+    FileSystemDirectoryContentChanged,
+    FileSystemDirectoryInitializationFailedAsPathDoesNotExist,
+    FileSystemDirectoryUnknownOperationForUnresolvedPath
 };
 
-typedef void(^FileObserverCallback)(FileSystemOperation);
-typedef void(^DirectoryObserverCallback)(FileSystemOperation);
+typedef void(^FileFileSystemObserverBlock)(FileSystemOperation);
+typedef void(^FileSystemObserverBlock)(FileSystemOperation, NSString *, NSString *);
 
 @interface FileSystemObserver : NSObject
 
-- (void)addObserverForPath:(NSString *)path fileObserver:(FileObserverCallback)fileBlock directoryObserver:(DirectoryObserverCallback)directoryBlock;
-
+- (void)addObserverForPath:(NSString *)path handler:(FileSystemObserverBlock)directoryBlock;
+- (void)replacePathForObserversFrom:(NSString *)originalPath To:(NSString *)newPath;
+- (NSString *)pathForBlock:(FileSystemObserverBlock)block;
+/*
+  Description: 
+        Remove the path and continue observing the other paths.
+ */
 - (void)removeObserverForPath:(NSString *)path;
+- (void)removeObserverForPath:(NSString *)path restartStream:(BOOL)restart;
 - (void)removeAllObservers;
+- (void)stopStream;
 
 @end
--- XCAssetGenerator/FileSystemObserver.m
+++ XCAssetGenerator/FileSystemObserver.m
@@ -1,15 +1,16 @@
 //
 //  FileSystemObserver.m
-//  XCAssetGenerator
+//  FSEventSwiftTest
 //
 //  Created by Bader on 10/10/14.
-//  Copyright (c) 2014 Pranav Shah. All rights reserved.
+//  Copyright (c) 2014 Bader. All rights reserved.
 //
 
 // ISSUES:
 // 1 - If you change the observable path, we wont observe it anymore. Example, if we observe ~/Downloads/Test and you rename it ~/Downloads/Test1 -> we will not observe it until you rename it back to the original.
 
 #import "FileSystemObserver.h"
+
 void fs_callback(ConstFSEventStreamRef ref, void * data, size_t events, void * paths, const FSEventStreamEventFlags flags[], const FSEventStreamEventId ids[]);
 
 NSString * const kFileObserverKey = @"FileObserverKey";
@@ -18,41 +19,108 @@ NSString * const kDirectoryObserverKey = @"DirectoryObserverKey";
 @interface FileSystemObserver ()
 @property (nonatomic, strong) NSMutableArray *pathsToObserve;
 @property (nonatomic, strong) NSMutableDictionary *observers;
+@property (nonatomic, strong) NSMutableDictionary *pathFileDescriptors;
 @property FSEventStreamRef fileStream;
-//@property
+
 @end
 
 @implementation FileSystemObserver
 
-#pragma mark - TODO:
--(id)init {
+#pragma mark - Initialization
+- (id)init
+{
     self = [super init];
     if (self == nil)
         return nil;
     
     _pathsToObserve = [[NSMutableArray alloc] init];
     _observers = [[NSMutableDictionary alloc] init];
+    _pathFileDescriptors = [[NSMutableDictionary alloc] init];
     
     return self;
 }
 
-- (void)addObserverForPath:(NSString *)path fileObserver:(FileObserverCallback)fileBlock directoryObserver:(DirectoryObserverCallback)directoryBlock {
+- (void)dealloc
+{
+    _observers = nil;
+    _pathsToObserve = nil;
+    
+    if (self.fileStream == NULL) return;
+    
+    FSEventStreamStop(self.fileStream);
+    FSEventStreamUnscheduleFromRunLoop(self.fileStream,
+                                       CFRunLoopGetCurrent(),
+                                       kCFRunLoopDefaultMode);
+    FSEventStreamInvalidate(self.fileStream);
+    FSEventStreamRelease(self.fileStream);
     
     
-    FSEventStreamContext context = { 0, (__bridge void *)self, NULL, NULL, NULL };
+    //    _fileStream = nil;
+}
+
+#pragma MARK - Observers Management
+
+- (void)addObserverForPath:(NSString *)path handler:(FileSystemObserverBlock)directoryBlock
+{
+    const char *cStringPath = [path cStringUsingEncoding:NSASCIIStringEncoding];
+    int fd = open(cStringPath, 0);
     
-    if (self.fileStream != NULL)
+    if (fd == -1) {
+        directoryBlock(FileSystemDirectoryInitializationFailedAsPathDoesNotExist, @"", @"");
+        return ;
+    }
+    
+    if (self.fileStream != NULL) {
+        
         [self invalidateStream:self.fileStream];
+    }
     
     if (![self.pathsToObserve containsObject:path]) {
         [self.pathsToObserve addObject:path];
     }
-    // Add the new observers to the existing observers
-    //    NSMutableArray *callbacks = [self.observers objectForKey:path];
-    //    [callbacks addObject:@{ @"file": fileBlock, @"directory": directoryBlock }];
     
-    [self.observers setValue:@{ kFileObserverKey: fileBlock, kDirectoryObserverKey: directoryBlock } forKey:path];
+    [self.pathFileDescriptors setObject:@(fd) forKey:path];
     
+    
+    [self.observers setValue:@{ kDirectoryObserverKey: directoryBlock } forKey:path];
+    
+    
+    [self createStream];
+    
+}
+
+
+- (void)removeObserverForPath:(NSString *)path
+{
+    [self removeObserverForPath:path restartStream:YES];
+}
+
+- (void)removeObserverForPath:(NSString *)path restartStream:(BOOL)restart
+{
+    [self.observers removeObjectForKey:path];
+    [self.pathsToObserve removeObject:path];
+    [self.pathFileDescriptors removeObjectForKey:path];
+    
+    if (restart)
+        [self restartStream];
+}
+
+- (void)removeAllObservers
+{
+    [self.pathsToObserve removeAllObjects];
+    [self.observers removeAllObjects];
+    [self.pathFileDescriptors removeAllObjects];
+    [self invalidateStream:self.fileStream];
+}
+
+
+#pragma MARK - Stream Managements
+
+
+- (void)createStream
+{
+    FSEventStreamContext context = { 0, (__bridge void *)self, NULL, NULL, NULL };
+
     self.fileStream = FSEventStreamCreate(NULL,
                                           &fs_callback,
                                           &context,
@@ -60,93 +128,130 @@ NSString * const kDirectoryObserverKey = @"DirectoryObserverKey";
                                           kFSEventStreamEventIdSinceNow,
                                           (CFAbsoluteTime)0.2,
                                           //                                          kFSEventStreamCreateFlagNone);
-                                          kFSEventStreamCreateFlagFileEvents);
+                                          kFSEventStreamCreateFlagWatchRoot);
     
     // start the stream on the main event loop
     FSEventStreamScheduleWithRunLoop(self.fileStream,
                                      CFRunLoopGetCurrent(),
                                      kCFRunLoopDefaultMode);
     FSEventStreamStart(self.fileStream);
-    //    NSLog(@"%u",self.fileStream);
     
 }
 
-- (void)removeObserverForPath:(NSString *)path {
-    [self.observers removeObjectForKey:path];
+- (void)restartStream
+{
+    if (self.fileStream != NULL)
+        [self invalidateStream:self.fileStream];
+    //    [self createStream];
 }
 
-- (void)removeAllObservers {
-    [self.pathsToObserve removeAllObjects];
-    [self.observers removeAllObjects];
+- (void)stopStream
+{
     [self invalidateStream:self.fileStream];
 }
 
-- (void)invalidateStream:(FSEventStreamRef)ref {
+- (void)invalidateStream:(FSEventStreamRef)ref
+{
+    if (ref == NULL) return;
+    
     FSEventStreamStop(ref);
-    FSEventStreamUnscheduleFromRunLoop(ref,
-                                       CFRunLoopGetCurrent(),
-                                       kCFRunLoopDefaultMode);
+    FSEventStreamUnscheduleFromRunLoop(ref, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
     FSEventStreamInvalidate(ref);
     FSEventStreamRelease(ref);
 }
 
+// Returns the main directory that we are observing -- given a subpath
+- (NSString *)observedPathForSubdirectory:(NSString *)subpath
+{
+    __block NSString *matchingPath = nil;
+    [self.pathsToObserve enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        if ([subpath containsString:obj]) {
+            matchingPath = obj;
+            *stop = YES;
+        }
+    }];
+    return matchingPath;
+}
 
-- (void)fileChangedForPath:(NSString *)path operation:(FileSystemOperation)operation {
+- (NSString *)parentDirectoryForSubdirectory:(NSString *)sub fromDirectories:(NSArray *)directories
+{
+    __block NSString *matchingPath = nil;
+    [directories enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        if ([sub containsString:obj]) {
+            matchingPath = obj;
+            *stop = YES;
+        }
+    }];
     
-    // General concept: Get all the entries for said path, (which is an array of dicitonaries containing all the callbacks.
-    // Then, enumate every dicitonary and trigger the appropriate callback
-    NSDictionary *observersForPath = [self.observers objectForKey:path];
-    FileObserverCallback callback = [observersForPath objectForKey:kFileObserverKey];
-    if (callback)
-        callback(operation);
-}
-
-- (void)directoryChangedForPath:(NSString *)path operation:(FileSystemOperation)operation {
+    return matchingPath;
     
-    // General concept: Get all the entries for said path, (which is an array of dicitonaries containing all the callbacks.
-    // Then, enumate every dicitonary and trigger the appropriate callback
-    NSDictionary *observersForPath = [self.observers objectForKey:path];
-    FileObserverCallback callback = [observersForPath objectForKey:kDirectoryObserverKey];
-    if (callback)
-        callback(operation);
 }
 
 
-
-- (void)dealloc {
-    FSEventStreamStop(self.fileStream);
-    FSEventStreamUnscheduleFromRunLoop(self.fileStream,
-                                       CFRunLoopGetCurrent(),
-                                       kCFRunLoopDefaultMode);
-    FSEventStreamInvalidate(self.fileStream);
-    FSEventStreamRelease(self.fileStream);
+- (void)replacePathForObserversFrom:(NSString *)originalPath To:(NSString *)newPath
+{
+    NSDictionary *observersOfOriginalPath = [self.observers objectForKey:originalPath];
+    if (observersOfOriginalPath != nil) {
+        [self removeObserverForPath:originalPath restartStream:NO];
+        [self addObserverForPath:newPath handler:[observersOfOriginalPath[kDirectoryObserverKey] copy]];
+    }
 }
-@end
 
 
 // TODO: Determine what to do with the callback. Seems like its gonna be a trial-and-error thing.
 void
-fs_callback(ConstFSEventStreamRef ref, void * data, size_t events, void * paths, const FSEventStreamEventFlags flags[], const FSEventStreamEventId ids[]) {
-    NSLog(@"Listening");
-    //   self()()()() = (__bridge self.class *?)userData;
+fs_callback(ConstFSEventStreamRef ref, void * data, size_t events, void * paths, const FSEventStreamEventFlags flags[], const FSEventStreamEventId ids[])
+{
+    FileSystemObserver *theObserver = (__bridge FileSystemObserver *)data;
     size_t	i;
     char ** _paths	= paths;
-    NSLog(@"number of events = %zu", events);
-    //    NSString * newName = [NSString stringWithFormat:@"%s", paths[0]];
+    NSString *path = [NSString stringWithFormat:@"%s", _paths[0]];
+    NSArray *pathsBeingObserved = (__bridge NSArray *)(FSEventStreamCopyPathsBeingWatched(ref));
     
-    //    NSLog(@"STAT: %u",fileStat);
-    
-    NSString * newName = [NSString stringWithFormat:@"%s", _paths[0]];
     for (i = 0; i < events; ++i) {
-        if (flags[i] & kFSEventStreamEventFlagItemRemoved) {
-            NSLog(@"%zu: Item removed:", i);
+        
+        if (flags[i] & kFSEventStreamEventFlagRootChanged) {
+            
+            //            NSString *thePath   = [theObserver observedPathForSubdirectory:path];
+            NSString *thePath = [theObserver parentDirectoryForSubdirectory:path fromDirectories:pathsBeingObserved];
+            int descriptor = ((NSNumber *)theObserver.pathFileDescriptors[thePath]).intValue;
+            char filePath[PATH_MAX];
+            
+            FileSystemObserverBlock block = [[theObserver.observers objectForKey:thePath] objectForKey:kDirectoryObserverKey];
+            
+            if (block) {
+                
+                if (fcntl(descriptor, F_GETPATH, filePath) != -1)
+                {
+                    NSString *newPath = [NSString stringWithUTF8String:filePath];
+                    BOOL isTrashed = [newPath containsString:@"/.Trash"];
+                    
+                    if (isTrashed)
+                        block(FileSystemDirectoryDeleted, path, newPath);
+                    else
+                        block(FileSystemDirectoryRenamed, path, newPath);
+                    
+                } else {
+                    // Cannot resolve the proper filde. Now what? BRAAAAAAPBUBUBUBUBPBRAAAAP *gun shots*
+                    block(FileSystemDirectoryUnknownOperationForUnresolvedPath, path, @"");
+                }
+                
+            }
         }
-        if (flags[i] & kFSEventStreamEventFlagItemRenamed) {
-            NSLog(@"%zu: Item renamed: %@", i, newName);
-        }
-        if (flags[i] & kFSEventStreamEventFlagItemCreated) {
-            NSLog(@"%zu: Item created: %@", i, newName);
-        }
-    }
+        
+        //        if (flags[i] & kFSEventStreamEventFlagItemRenamed) {}
+        
+        //        if (flags[i] & kFSEventStreamEventFlagItemRemoved) {}
+        
+        //        if (flags[i] & kFSEventStreamEventFlagItemCreated) {}
+        
+        //        if (flags[i] & kFSEventStreamEventFlagItemIsDir) {}
+        
+        //        if (flags[i] & kFSEventStreamEventFlagItemIsFile) {}
+        
+    } // End Events loop
+    
+    
     
 }
+@end
