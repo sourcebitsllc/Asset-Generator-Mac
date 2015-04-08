3ecd7ef5c378d6bf6d4fad304701b936aa2e6bab
Bader <b.h.alrazzaqa@hotmail.com>
2015-04-08 09:58:59 +0300

    Move retreival functions into new group + small changes related to the previous additions

---
+++ XCAssetGenerator/PathQuery.swift
@@ -0,0 +1,43 @@
+//
+//  PathQuery.swift
+//  XCAssetGenerator
+//
+//  Created by Bader on 4/5/15.
+//  Copyright (c) 2015 Pranav Shah. All rights reserved.
+//
+
+import Foundation
+
+struct PathQuery {
+    ///
+    /// TODO: Documentation
+    ///
+    static func availableImages(from path: Path) -> [Path] {
+        return queryWith(path, searchOption: NSDirectoryEnumerationOptions.SkipsHiddenFiles) { element -> Path? in
+            let isImage = element.path!.hasSuffix(".png") || element.path!.hasSuffix(".jpg") || element.path!.hasSuffix(".jpeg")
+            return (isImage) ? element.path! : nil
+        }
+    }
+    
+    static func availableAssetFolders(from path: Path) -> [Path] {
+        return queryWith(path, searchOption: NSDirectoryEnumerationOptions.SkipsPackageDescendants) { element -> Path? in
+            let isAssetFolder = element.path!.hasSuffix(".imageset") || element.path!.hasSuffix(".appiconset") || element.path!.hasSuffix(".launchimage")
+            return (isAssetFolder) ? element.path! : nil
+        }
+    }
+    
+    private static func queryWith<T>(path: Path,  searchOption: NSDirectoryEnumerationOptions, query: (NSURL -> T?)) -> [T] {
+        let url = NSURL(fileURLWithPath: path, isDirectory: true)
+        let generator = NSFileManager.defaultManager().enumeratorAtURL(url!, includingPropertiesForKeys: [NSURLIsDirectoryKey], options: searchOption , errorHandler: nil)
+        var matches = [T]()
+        
+        while let element = generator?.nextObject() as? NSURL {
+            let result = query(element)
+            if let result = result {
+                matches.append(result)
+            }
+        }
+        return matches
+    }
+    
+}
--- XCAssetGenerator/Validator.swift
+++ XCAssetGenerator/Validator.swift
@@ -14,7 +14,7 @@ extension BookmarkResolver: Validator {
     
     class func isBookmarkValid(bookmark: Bookmark?) -> Bool {
         if let b = bookmark {
-            let path: Path? = self.resolvePathFromBookmark(b)
+            let path: Path? = resolvePathFromBookmark(b)
             return (path != nil) ? PathValidator.directoryExists(path: path!) : false
         } else {
             return false
@@ -32,17 +32,13 @@ class ProjectValidator: Validator {
         return BookmarkResolver.isBookmarkValid(project.assetBookmark)
     }
     
-    class func isAssetValid(asset: AssetFolder) -> Bool {
+    class func isAssetValid(asset: AssetsFolder) -> Bool {
         return BookmarkResolver.isBookmarkValid(asset.bookmark)
     }
 }
 
 
 class PathValidator: Validator {
-    
-    /* 
-        NOTE: The renaming should be done directly from the main bash script (ScriptExecutor)
-    */
     
     class func directoryContainsInvalidCharacters(#path: Path, options: AnyObject?) -> Bool {
         return directoryWith(path, searchOption: NSDirectoryEnumerationOptions.SkipsHiddenFiles) { (url, isDirectory) -> Bool? in
@@ -57,38 +53,7 @@ class PathValidator: Validator {
             return nil
         } ?? false
     }
-    
-    class func images(path: Path) -> [Path] {
-        let url = NSURL(fileURLWithPath: path, isDirectory: true)
-        
-        let generator = NSFileManager.defaultManager().enumeratorAtURL(url!, includingPropertiesForKeys: [NSURLIsDirectoryKey], options: NSDirectoryEnumerationOptions.SkipsHiddenFiles , errorHandler: nil)
-        
-        var foundImages = [Path]()
-        while let element = generator?.nextObject() as? NSURL {
-            let isImage = element.path!.hasSuffix(".png") || element.path!.hasSuffix(".jpg") || element.path!.hasSuffix(".jpeg")
 
-            if isImage { foundImages.append(element.path!) }
-        }
-        
-        return foundImages
-    }
-    
-    
-    class func xcassetFolders(path: Path) -> [Path] {
-        let url = NSURL(fileURLWithPath: path, isDirectory: true)
-        
-        let generator = NSFileManager.defaultManager().enumeratorAtURL(url!, includingPropertiesForKeys: [NSURLIsDirectoryKey], options: NSDirectoryEnumerationOptions.SkipsPackageDescendants , errorHandler: nil)
-        
-        var foundFolders = [Path]()
-        while let element = generator?.nextObject() as? NSURL {
-            let isAssetFolder = element.path!.hasSuffix(".imageset") || element.path!.hasSuffix(".appiconset") || element.path!.hasSuffix(".launchimage")
-            
-            if isAssetFolder { foundFolders.append(element.path!) }
-        }
-        
-        return foundFolders
-    }
-    
     
     class func retreiveProject(url: NSURL) -> NSURL? {
         return directoryWith(url.path!, searchOption: NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants) { (url, isDirectory) -> NSURL? in
@@ -106,14 +71,7 @@ class PathValidator: Validator {
         } ?? false
     }
     
-    /*
-        @param:
-            f: (NSURL) -> T?  ;; If result of applying closure is nil, we will not return it but will skip instead and proceed with the generator loop.
-                                Additionally, accessing the path component of the NSURL is always safe. (Kinda unintuitive API right now though)
-                                We use the optional return type to be able to skip non-satisfying results and still be able to proceed with loop.
-        @return type
-            We use an optional return type to encompass all our return needs.
-    */
+
     private class func directoryWith<T>(path: Path, searchOption: NSDirectoryEnumerationOptions,  f: (NSURL, Bool) -> T?) -> T? {
         let url = NSURL(fileURLWithPath: path, isDirectory: true)
         
