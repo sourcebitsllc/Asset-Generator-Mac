e3895fd1cb071d9b92cbc83f954d86e3dc90e556
Bader <b.h.alrazzaqa@hotmail.com>
2015-07-02 04:07:43 +0300

    Better Status for previous commit

--- XCAssetGenerator/StatusCrafter.swift
+++ XCAssetGenerator/StatusCrafter.swift
@@ -22,7 +22,7 @@ struct StatusCrafter {
         case (.None, _):
             return "Drop a folder with slices you'd like to add to your Xcode project."
         case (.Some(let a), .None):
-            let end = (a.count > 0) ? "\(a.count) assets to." : "assets to."
+            let end = (a.count > 0) ? "\(a.count) assets" : "assets"
             return "Choose an Xcode project to add " + end
         case (.Some(let a), .Some(let catalog)) where a.count == 0:
             return "Add slices to the folder in order to build assets."
