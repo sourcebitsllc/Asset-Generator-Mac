b42c5a6f0fb15d2d93311ed5210da13418208aa5
Bader <b.h.alrazzaqa@hotmail.com>
2015-07-06 00:16:14 +0300

    Use new content equality API for `diff`ing assets

--- XCAssetGenerator/AssetDiff.swift
+++ XCAssetGenerator/AssetDiff.swift
@@ -19,13 +19,11 @@ struct AssetDiff {
         case MissingAssets
     }
     
-    //    private static let f = NSFileManager()
-    
     static func diffWithOperation(source: [Asset], catalog: AssetCatalog)(operation: DiffOperation) -> Int {
-        let s = source.map { AssetDiff.Comparison(path: $0.fullPath, key: $0.fullPath.remove([$0.ancestor.removeTrailingSlash()])) }
+        let s = source.map { Comparison(path: $0.fullPath, key: $0.fullPath.remove([$0.ancestor.removeTrailingSlash()])) }
         
         let c = PathQuery.availableImages(from: catalog.path).map { path in
-            return AssetDiff.Comparison(path: path, key: path.remove([catalog.path]).removeAssetSetsComponent())
+            return Comparison(path: path, key: path.remove([catalog.path]).removeAssetSetsComponent())
         }
         
         switch operation {
@@ -46,27 +44,25 @@ struct AssetDiff {
     }
     
     private static func common(source: [Comparison], target: [Comparison]) -> Int {
-        let f = NSFileManager()
         var total = 0
         for c in target {
-            total += source.filter { $0.key == c.key && f.contentsEqualAtPath($0.path, andPath: c.path) }.count
+            total += source.filter { $0.key == c.key && FileSystem.equal($0.path, c.path) }.count
         }
         return total
     }
     
     private static func new(source: [Comparison], target: [Comparison]) -> Int {
         var new = 0
-        let f = NSFileManager()
-        // Inefficient. I'm too lazy to write an imparative loop right now. TODO: Swift 2.0, indexOf {}
+        // Inefficient. I'm too lazy to write an imparative loop right now. // TODO: Swift 2.0, indexOf {}
         for s in source {
-            new += target.filter { $0.key == s.key && f.contentsEqualAtPath($0.path, andPath: s.path) }.count == 0 ? 1 : 0
+            new += target.filter { $0.key == s.key && FileSystem.equal($0.path, s.path) }.count == 0 ? 1 : 0
         }
         return new
     }
     
     private static func missing(source: [Comparison], target: [Comparison]) -> Int {
         var new = 0
-        // Inefficient. I'm too lazy to write an imparative loop right now. TODO: Swift 2.0, indexOf {}
+        // Inefficient. I'm too lazy to write an imparative loop right now. // TODO: Swift 2.0, indexOf {}
         for s in target {
             new += source.filter { $0.key == s.key }.count == 0 ? 1 : 0
         }
