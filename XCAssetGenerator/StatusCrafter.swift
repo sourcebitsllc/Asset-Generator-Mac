a51fd56926dcdf7a92e40e2134cf6f7b3b0af0b0
Bader <b.h.alrazzaqa@hotmail.com>
2015-07-05 04:36:32 +0300

    Add a dot at the end of Status

--- XCAssetGenerator/StatusCrafter.swift
+++ XCAssetGenerator/StatusCrafter.swift
@@ -23,7 +23,7 @@ struct StatusCrafter {
             return "Drop a folder with slices you'd like to add to your Xcode project."
         case (.Some(let a), .None):
             let end = (a.count > 0) ? "\(a.count) assets" : "assets"
-            return "Choose an Xcode project to add " + end
+            return "Choose an Xcode project to add " + end + "."
         case (.Some(let a), .Some(let catalog)) where a.count == 0:
             return "Add slices to the folder in order to build assets."
         case (.Some(let a), .Some(let catalog)):
